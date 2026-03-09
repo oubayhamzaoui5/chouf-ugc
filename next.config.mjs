1db60472cb1f19c394716037e64befaa4201d5e1
oubayhamzaoui5 <hamzaouioubay@isamm3u-manouba.tn>
2026-03-09 05:01:10 +0100

    Configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production"
+const repoName = "chouf-ugc"
+
 const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `/${repoName}/` : "",
   typescript: {
     ignoreBuildErrors: true,
   },
